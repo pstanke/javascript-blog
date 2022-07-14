4cd37b22e3da10df97fc6ecbc26540d38c3ee983
Pawel Stanke <pablostanke21@gmail.com>
2022-07-14 21:54:35 +0200

    Update js

--- js/script.js
+++ js/script.js
@@ -88,13 +88,13 @@
   function generateTags() {
     /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
-
+    console.log(articles);
     /* START LOOP: for every article: */
 
     for (let article of articles) {
       /* find tags wrapper */
-      const tagsWrapper = article.querySelectorAll(optArticleTagsSelector);
-
+      const tagsWrapper = article.querySelector(optArticleTagsSelector);
+      console.log(tagsWrapper);
       /* make html variable with empty string */
       let html = '';
 
